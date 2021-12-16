9ed8762c40213972eab890ca6e5fb6895b5cac10
zhu00121 <48067384+zhu00121@users.noreply.github.com>
2021-12-16 14:19:30 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='am_analysis',
       version='0.1',
       description='Amplitude Modulation Analysis Module',
-      url='https://github.com/zhu00121/amplitude-modulation-analysis-module',
+      url='https://github.com/MuSAELab/amplitude-modulation-analysis-module',
       author='Raymundo Cassani and João Monteio',
       author_email='raymundo.cassani@gmail.com',
       license='MIT',
